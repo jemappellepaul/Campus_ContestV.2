035b510894b89e82f09a9e16864aeeec1cca3bb9
Sazulcosplay <56249744+Sazulcosplay@users.noreply.github.com>
2020-02-18 14:34:18 +0100

    a lot of modifications I've Had

--- laravel-cc/resources/views/auth/login.blade.php
+++ laravel-cc/resources/views/auth/login.blade.php
@@ -31,7 +31,7 @@
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
-                                @error('password')
+                                <br>@error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
--- laravel-cc/resources/views/auth/verify.blade.php
+++ laravel-cc/resources/views/auth/verify.blade.php
@@ -5,20 +5,20 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header">{{ __('Vérifiez votre adrese email') }}</div>
 
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            {{ __('un mail viens de vous être envoyé à votre adresse email.') }}
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
+                    {{ __('veuillez vérifier votre adresse email part le lien de confirmation.') }}
                     {{ __('If you did not receive the email') }},
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('Cliquez ici pour recevoir une autre confirmation.') }}</button>.
                     </form>
                 </div>
             </div>
